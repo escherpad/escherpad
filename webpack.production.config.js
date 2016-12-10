440fc7e5e42488c720222827a75b14c961fb4453
Ge Yang <yangge1987@gmail.com>
2016-12-10 07:20:41 -0600

    BUILD update

--- webpack.production.config.js
+++ webpack.production.config.js
@@ -94,7 +94,7 @@ module.exports = {
     new webpack.DefinePlugin({'process.env.NODE_ENV': JSON.stringify('production')}),
     new CopyWebpackPlugin([
       {from: 'src/index.html'},
-      {from: 'src/oauth'}
+      {from: 'src/oauth', to: 'oauth'}
     ]),
     new webpack.optimize.UglifyJsPlugin({compress: {warnings: false}}),
     new webpack.optimize.OccurenceOrderPlugin(),
