85401cb9768169b5f69450a7ca0b21db95e329ea
Ge Yang <yangge1987@gmail.com>
2016-12-10 07:09:12 -0600

    use window.location.href for the redirect

--- src/modules/dropbox.js
+++ src/modules/dropbox.js
@@ -3,7 +3,7 @@
 import DropboxApi, {parseTokenQueryString, requestAccessToken} from "eywa-dropbox";
 
 // export const REDIRECT_URI = "http://localhost:4000/gittor/oauth/dropbox-redirect.html";
-export const REDIRECT_URI = "http://episodeyang.com/gittor/oauth/dropbox-redirect.html";
+export const REDIRECT_URI = `${window.location.href/oauth/}dropbox-redirect.html`;
 export const CLIENT_ID = "kroubcx0l6slseu";
 
 const dapi = new DropboxApi(CLIENT_ID, REDIRECT_URI);
