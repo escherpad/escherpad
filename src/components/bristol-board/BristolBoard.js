aeb933431b505d7b6ce0fc7dd0e2956ade6fc968
Ge Yang <yangge1987@gmail.com>
2016-12-16 10:27:31 -0600

    increase stroke width

--- src/components/bristol-board/BristolBoard.js
+++ src/components/bristol-board/BristolBoard.js
@@ -22,7 +22,7 @@ class BristolBoard extends Component {
   };
 
   componentWillMount(){
-    this.setState({pen: {type: "SimplePen", color: '#003BFF', strokeWidth: 1.4}});
+    this.setState({pen: {type: "SimplePen", color: '#003BFF', strokeWidth: 2}});
   }
 
   @autobind
@@ -72,7 +72,7 @@ class BristolBoard extends Component {
           </FlexItem>
           <FlexItem fixed className="bristol-toolbar">
             <button className="select-pen"
-                    onClick={this.select({type: "SimplePen", color: '#003BFF', strokeWidth: 1.4})}>
+                    onClick={this.select({type: "SimplePen", color: '#003BFF', strokeWidth: 2})}>
               <i className="material-icons">edit</i>
             </button>
             <button className="select-eraser" onClick={this.select({type: "Eraser", alpha: 0.5, strokeWidth: 20})}>
