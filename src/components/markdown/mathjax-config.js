e51e79587d662ad80a4864878559c993b75fcf53
Ge Yang <yangge1987@gmail.com>
2017-02-23 21:39:01 -0600

    fix label collision error when using references.
    fixes this issue https://github.com/mathjax/MathJax/issues/1705

--- src/components/markdown/mathjax-config.js
+++ src/components/markdown/mathjax-config.js
@@ -66,14 +66,39 @@ function configureMathjax() {
       disabled: true
     }
   });
+  MathJax.Hub.Register.MessageHook("Error", function (message) {
+    //  do something with the error.  message[2] is the Error object that records the problem.
+    console.log(message);
+  });
+  MathJax.Hub.Register.MessageHook("Begin Math Input", function () {
+    MathJax.Extension["TeX/AMSmath"].refs = [];
+    MathJax.Extension["TeX/AMSmath"].refUpdate = false
+  });
+  // todo: add end math input, compare what label has changed, save label to global via the index.
+  // label number is stil the same. tag is start
   MathJax.Hub.Configured();
 }
 
+
+function removeKey(obj, f, once = false) {
+  "use strict";
+  const ks = Object.keys(obj);
+  for (let i in ks) {
+    let key = ks[i];
+    if (f(obj[key])) {
+      delete obj[key];
+      if (once) return;
+    }
+  }
+}
 export function resetNumbering(start = 1) {
   "use strict";
   /* see this link: https://github.com/benweet/stackedit/issues/180 */
   /* note: to change format of equation labeling, take a look at this link
-  * http://tex.stackexchange.com/questions/63138/how-do-i-number-equations-with-roman-numbers */
+   * http://tex.stackexchange.com/questions/63138/how-do-i-number-equations-with-roman-numbers */
   window.MathJax.Extension["TeX/AMSmath"].startNumber = start - 1;
-  // window.MathJax.Extension["TeX/AMSmath"].labels = {};
+  // done: scan through the tags, figure out what tag this corresponds to, remove the key that tag correspond to.
+  // link: https://github.com/mathjax/MathJax/issues/1705
+  // notice: assume only one equation occurs in each render.
+  removeKey(window.MathJax.Extension["TeX/AMSmath"].labels, (v) => (Number(v.tag) === start), true);
 }
