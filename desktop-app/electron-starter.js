47b38fdb24e5406d3c41e4148c8f68c9a97395f3
Ge Yang <yangge1987@gmail.com>
2017-03-31 03:13:39 -0500

    got electron + react to work

--- desktop-app/electron-starter.js
+++ desktop-app/electron-starter.js
@@ -1,24 +1,26 @@
-const {app, BrowserWindow} = require('electron')
-const path = require('path')
-const url = require('url')
+const {app, BrowserWindow} = require('electron');
+const path = require('path');
+const url = require('url');
 
 // Keep a global reference of the window object, if you don't, the window will
-// // be closed automatically when the JavaScript object is garbage collected.
-// let win
-//
+// be closed automatically when the JavaScript object is garbage collected.
+let win;
+
 function createWindow() {
     // Create the browser window.
-    win = new BrowserWindow({width: 800, height: 600})
+    win = new BrowserWindow({width: 800, height: 600});
 
     // and load the index.html of the app.
-    win.loadURL(url.format({
-        pathname: path.join(__dirname, 'index.html'),
-        protocol: 'file:',
-        slashes: true
-    }))
+    // from https://medium.freecodecamp.com/building-an-electron-application-with-create-react-app-97945861647c
+    const startUrl = process.env.ELECTRON_START_URL || url.format({
+            pathname: path.join(__dirname, 'build/index.html'),
+            protocol: 'file:',
+            slashes: true
+        });
+    win.loadURL(startUrl);
 
     // Open the DevTools.
-    win.webContents.openDevTools()
+    win.webContents.openDevTools();
 
     // Emitted when the window is closed.
     win.on('closed', () => {
@@ -32,7 +34,7 @@ function createWindow() {
 // This method will be called when Electron has finished
 // initialization and is ready to create browser windows.
 // Some APIs can only be used after this event occurs.
-app.on('ready', createWindow)
+app.on('ready', createWindow);
 
 // Quit when all windows are closed.
 app.on('window-all-closed', () => {
@@ -41,7 +43,7 @@ app.on('window-all-closed', () => {
     if (process.platform !== 'darwin') {
         app.quit()
     }
-})
+});
 
 app.on('activate', () => {
     // On macOS it's common to re-create a window in the app when the
@@ -49,8 +51,9 @@ app.on('activate', () => {
     if (win === null) {
         createWindow()
     }
-})
+});
 
 // In this file you can include the rest of your app's specific main process
 // code. You can also put them in separate files and require them here.
+
 
