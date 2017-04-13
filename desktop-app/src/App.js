5e4ded0f63ab6f837f040ec8cfb50c1ac8d15d64
Ge Yang <yangge1987@gmail.com>
2017-04-13 18:59:34 -0500

    WIP simlink es6 transpilation not working

--- desktop-app/src/App.js
+++ desktop-app/src/App.js
@@ -1,6 +1,8 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
+import {store$} from "./client-common/model";
+console.log(store$);
 
 class App extends Component {
   render() {
