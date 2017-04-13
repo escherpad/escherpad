9574373afdb5eb7cb907792d2642d415bb5c82ac
Ge Yang <yangge1987@gmail.com>
2017-04-13 18:51:18 -0500

    add simlink to client-common

--- desktop-app/electron-starter.js
+++ desktop-app/electron-starter.js
@@ -5,7 +5,6 @@ const url = require('url');
 // Keep a global reference of the window object, if you don't, the window will
 // be closed automatically when the JavaScript object is garbage collected.
 let win;
-
 function createWindow() {
     // Create the browser window.
     win = new BrowserWindow({width: 800, height: 600});
