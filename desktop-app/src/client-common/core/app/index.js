412de2717484405c235c44af8c41963ca71d457d
Ge Yang <yangge1987@gmail.com>
2017-04-22 16:35:18 -0500

    framework WIP, fix gittor

---
+++ desktop-app/src/client-common/core/app/index.js
@@ -0,0 +1,10 @@
+// escherpad has all these insertion points
+NAME_SPACE = "example-plugin";
+// this is almost DI.
+export const module = {
+    commands: {},
+    reducer: {}, // or a function (s, a)=>s
+    views: {},
+    viewAnchors: {},
+    saga: {}
+};
