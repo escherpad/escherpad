19c195a5115b434945dbe9033e3986af5616932c
Ge Yang <yangge1987@gmail.com>
2017-04-22 16:36:37 -0500

    framework WIP

--- core/app/index.js
+++ core/app/index.js
@@ -2,9 +2,11 @@
 NAME_SPACE = "example-plugin";
 // this is almost DI.
 export const module = {
+    requireReload: true,
     commands: {},
     reducer: {}, // or a function (s, a)=>s
     views: {},
     viewAnchors: {},
-    saga: {}
+    saga: {},
+    config: (c) => c
 };
