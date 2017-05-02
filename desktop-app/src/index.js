e6f4ca4116d0b699eaf0d433599c9834e9ea495d
Ge Yang <yangge1987@gmail.com>
2017-05-02 02:16:27 -0500

    feat: FileViewer scaffold

--- desktop-app/src/index.js
+++ desktop-app/src/index.js
@@ -4,6 +4,7 @@ const ra_ = new Ra_();
 ra_.loadModule(require('./client-common/core/app').default);
 ra_.loadModule(require('./client-common/core/context-view').default);
 ra_.loadModule(require('./client-common/core/file-list-view').default);
+ra_.loadModule(require('./client-common/core/file-viewer').default);
 
 window.document.addEventListener('DOMContentLoaded', function () {
     ra_.bootstrap(window.document.querySelector('#bootstrap'));
