481f0a0d048b3ab4994e8263a3204d27400c96e4
Ge Yang <yangge1987@gmail.com>
2017-05-01 21:55:22 -0500

    [feat]: add file-list-view component

--- core/file-list-view/index.js
+++ core/file-list-view/index.js
@@ -1,6 +1,5 @@
 // escherpad has all these insertion points
 const NAMESPACE = "core-app";
-import Bootstrap from "./bootstrap"
 
 // this is almost DI.
 export default {
@@ -8,9 +7,7 @@ export default {
     requireReload: true,
     commands: {},
     reducer: {}, // or a function (s, a)=>s
-    views: {
-        _bootstrap: Bootstrap
-    },
+    views: {},
     viewAnchors: {},
     saga: {},
     config: (c) => c
