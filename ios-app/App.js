1fe03f599d80fb15f1d1b015591c8eaf9cd80a19
jamworld <chencha92111@gmail.com>
2017-05-02 03:12:37 -0500

    test ios app

--- ios-app/App.js
+++ ios-app/App.js
@@ -1,12 +1,13 @@
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
+import Ra_ from "./client-common/Ra_"
 
 export default class App extends React.Component {
   render() {
     return (
       <View style={styles.container}>
         <Text>Open up App.js to start working on your app!</Text>
-        <Text>Changes you make will automatically reload.</Text>
+        <Text>hi you make will automatically reload.</Text>
         <Text>Shake your phone to open the developer menu.</Text>
       </View>
     );
@@ -21,3 +22,7 @@ const styles = StyleSheet.create({
     justifyContent: 'center',
   },
 });
+
+
+
+
