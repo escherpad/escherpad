c2299591d3207f6fdbb40d2998a98e867a02e2fc
Ge Yang <yangge1987@gmail.com>
2017-05-01 22:01:02 -0500

    doc: update doc string

--- core/file-list-view/index.js
+++ core/file-list-view/index.js
@@ -3,14 +3,18 @@ const NAMESPACE = "file-list-view";
 
 // this is almost DI.
 export default {
+    /** Module Parameters*/
     namespace: NAMESPACE,
     requireReload: true,
+
+    /** Model and Controller Code */
     commands: {},
     reducers: {}, // or a function (s, a)=>s
+    sagas: {},
 
+    /** View Code */
     // publish a list of view anchors
     viewAnchors: [],
     views: {},
-    sagas: {},
     config: (c) => c
 }
