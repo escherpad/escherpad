1d42403ac4bfcd0030e7c3a90d01f1854c8ae8d4
Ge Yang <yangge1987@gmail.com>
2017-05-01 19:02:57 -0500

    WIP, trying to add flowtype, but keeps erroring ou

--- desktop-app/src/App.js
+++ desktop-app/src/App.js
@@ -1,13 +1,14 @@
-import React, { Component } from 'react';
+// @flow
+import React, {Component} from 'react';
 
 class App extends Component {
-  render() {
-    return (
-      <div className="App">
-          <h1>hahaha</h1>
-      </div>
-    );
-  }
+    render() {
+        return (
+            <div className="App">
+                <h1>hahaha</h1>
+            </div>
+        );
+    }
 }
 
 export default App;
