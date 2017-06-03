ff9a419bedcce9556460ae0e409726709966c48b
Ge Yang <yangge1987@gmail.com>
2017-06-03 14:46:52 -0500

    add doc on packages folder

---
+++ file-browser/src/model.js
@@ -0,0 +1,6 @@
+import React from 'react';
+import ReactDOM from "react-dom";
+
+window.document.addEventListener('DOMContentLoaded', function () {
+    ReactDOM.render(<div/>, window.document.querySelector('#bootstrap'));
+});
