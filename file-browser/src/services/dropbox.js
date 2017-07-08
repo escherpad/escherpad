24c1080d4f9b336ed2bf9fc69e8f851d0a8f34ef
jamworld <chencha92111@gmail.com>
2017-07-08 00:33:37 -0500

    [ADD] dropbox account helper

--- file-browser/src/services/dropbox.js
+++ file-browser/src/services/dropbox.js
@@ -27,6 +27,10 @@ import {
     fork
 } from "luna-saga";
 
+// test only
+const testClientId : string = "uhpeu63dv1pqgor";
+const testAccessToken : string = "p5mHBr4rHvYAAAAAAAAANpuR45x2fOWdBSQqs5rtKeFD8GMF7uH8emUZTy_F53zT";
+
 export function* dropboxMainProc() {
     // while (true) {
     // console.log(fork);
@@ -34,4 +38,21 @@ export function* dropboxMainProc() {
     // console.log(result);
     // fork(new_process, arguments1, arg2, arg3...);
     // }
+}
+
+
+const uuidV4 = require('uuid/v4');
+export function accountHelper(cliendId = testClientId, accessToken = testAccessToken, uuidGenerator = uuidV4) {
+    return {
+        $key: uuidGenerator(),
+        client_id: cliendId,
+        access_token: accessToken,
+        backend: 'dropbox',
+        view: {
+            user_id: 'test userId',
+            username: 'jam-world',
+            email: 'chencha92111@gmail.com',
+            name: 'jamworld'
+        }
+    }
 }
