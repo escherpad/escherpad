8f17f2d76b8da7df0dfa94abcfdf9d3f27e5cdf1
jamworld <chencha92111@gmail.com>
2017-07-09 22:51:57 -0500

    [UPDATE] use the hard coded account info from model

--- file-browser/src/services/github.js
+++ file-browser/src/services/github.js
@@ -3,22 +3,6 @@ import {
     GitHubAPI
 } from "eywa";
 
-/* test only code*/
-// all of the tests below require OAuth to work.
-import {
-    githubClientId as clientId,
-    githubClientSecret,
-    githubAccessToken as accessToken,
-    testRepoName,
-    testGithubUsername as testUsername,
-    testPublickRepo,
-    testPrivateRepo
-} from "../test.config.js";
-/* end*/
-
-
-
-
 const COMMANDS = [
     "GITHUB::LIST_FILES",
     "GITHUB::LIST_REPOS",
@@ -33,46 +17,38 @@ import {
     dispatch
 } from "luna-saga";
 
-// test only
-const testClientId : string = "b4666a3b2ac86229f0c7";
-const testAccessToken : string = "f7ccfee5aa99defa67b713dea00f739d0be5939c";
-
-let gh = new GitHubAPI(clientId);
-gh.updateAccessToken(accessToken);
-
-console.log("Init github");
-console.log(gh);
-
 export function* githubMainProc() {
     while (true) {
         let action = yield take("GITHUB::LIST_FILES");
-        console.log("github main process");
-        console.log(action, gh);
-        const data = yield gh.listRepos(testUsername);
-        console.log("list repos from the github");
-        console.log(data);
-        let repoNames = data.map((metaInfo) => {
-            return {id: metaInfo.id, full_name: metaInfo.full_name};
-        });
-        console.log("get the repo names");
-        console.log(repoNames);
-        yield dispatch({type: "DISPLAY_FILES", data: repoNames});
-    }
-}
+
+        console.log("GITHUB::LIST_FILES");
+        console.log(action);
+        for (let account in action.state.accounts) {
+            let info = account.split(":");
+            if (info[0] == "github") {
+                let username = info[1];
+                let account_info = action.state.accounts[account];
+                let client_id = account_info.client_id;
+                let access_token = account_info.access_token;
 
 
-const uuidV4 = require('uuid/v4');
-export function accountHelper(clientId = testClientId, accessToken = testAccessToken, uuidGenerator = uuidV4) {
-    return {
-        $key: uuidGenerator(),
-        client_id: testClientId,
-        access_token: testAccessToken,
-        backend: 'github',
-        view: {
-            user_id: 'test userId',
-            username: 'jam-world',
-            email: 'chencha92111@gmail.com',
-            name: 'jamworld'
+                let gh = new GitHubAPI(client_id);
+                gh.updateAccessToken(access_token);
+
+                console.log("github main process");
+                console.log(action, gh);
+                const data = yield gh.listRepos(username);
+                console.log("list repos from the github");
+                console.log(data);
+                let repoNames = data.map((metaInfo) => {
+                    return {id: metaInfo.id, full_name: metaInfo.full_name};
+                });
+                console.log("get the repo names");
+                console.log(repoNames);
+                yield dispatch({type: "DISPLAY_FILES", data: repoNames});
+            }
         }
     }
 }
+
+
