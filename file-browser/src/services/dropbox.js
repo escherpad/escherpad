d7b739059b322249091ac0cbbeab384a522250f4
jamworld <chencha92111@gmail.com>
2017-07-09 22:52:20 -0500

    [UPDATE] remove the hard coded account info

--- file-browser/src/services/dropbox.js
+++ file-browser/src/services/dropbox.js
@@ -27,10 +27,6 @@ import {
     fork
 } from "luna-saga";
 
-// test only
-const testClientId : string = "uhpeu63dv1pqgor";
-const testAccessToken : string = "p5mHBr4rHvYAAAAAAAAANpuR45x2fOWdBSQqs5rtKeFD8GMF7uH8emUZTy_F53zT";
-
 export function* dropboxMainProc() {
     // while (true) {
     // console.log(fork);
@@ -41,18 +37,3 @@ export function* dropboxMainProc() {
 }
 
 
-const uuidV4 = require('uuid/v4');
-export function accountHelper(cliendId = testClientId, accessToken = testAccessToken, uuidGenerator = uuidV4) {
-    return {
-        $key: uuidGenerator(),
-        client_id: cliendId,
-        access_token: accessToken,
-        backend: 'dropbox',
-        view: {
-            user_id: 'test userId',
-            username: 'jam-world',
-            email: 'chencha92111@gmail.com',
-            name: 'jamworld'
-        }
-    }
-}
