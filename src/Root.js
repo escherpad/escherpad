dfa3553adce56df051d97e859f7b40d3c25d3f91
Ge Yang <yangge1987@gmail.com>
2017-07-23 01:31:57 -0500

    add wildcard to route

--- src/Root.js
+++ src/Root.js
@@ -1,15 +1,18 @@
 import React from "react";
-import {Route} from "react-router-dom";
+import {Route, Switch} from "react-router-dom";
+
+// dev only
+import JSONPretty from 'react-json-pretty';
+
 
 export default function Root() {
     return (
-        <div>
+        <Switch>
             <Route exact path="/" component={() => <div>path is /</div>}/>
             <Route exact path="/all" component={() => <div>path is /all</div>}/>
-            <Route exact path="/github/:username/:repo?:path" component={() => <div>path is /github/:username/:repo/:path</div>}/>
-            <Route exact path="/dropbox/:path" component={() => <div>root page</div>}/>
-            <Route exact path="/jupyter/:path" component={() => <div>root page</div>}/>
-            <Route exact path="/bindrs/:bindr_id/:note_id" component={() => <div>root page</div>}/>
-        </div>
+            <Route exact path="/:username/:bindr_id/:path*"
+                   component={(props) => <div>root page<JSONPretty json={props}/></div>}/>
+            <Route component={()=><div>404 no match</div>}/>
+        </Switch>
     );
 }
