204f9b172408ab1a01ba3d58dcb6c45fe5691d9c
Ge Yang <yangge1987@gmail.com>
2017-07-23 01:13:05 -0500

    add routes

--- src/Root.js
+++ src/Root.js
@@ -3,6 +3,13 @@ import {Route} from "react-router-dom";
 
 export default function Root() {
     return (
-        <Route exact path="/" component={() => <div>root page</div>}/>
+        <div>
+            <Route exact path="/" component={() => <div>path is /</div>}/>
+            <Route exact path="/all" component={() => <div>path is /all</div>}/>
+            <Route exact path="/github/:username/:repo?:path" component={() => <div>path is /github/:username/:repo/:path</div>}/>
+            <Route exact path="/dropbox/:path" component={() => <div>root page</div>}/>
+            <Route exact path="/jupyter/:path" component={() => <div>root page</div>}/>
+            <Route exact path="/escherpad/:bindr_id/:note_id" component={() => <div>root page</div>}/>
+        </div>
     );
 }
