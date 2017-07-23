2138117f15e69d73d1ba9fcb95aefa4ed76cfbfe
Ge Yang <yangge1987@gmail.com>
2017-07-23 01:57:19 -0500

    update path

--- web-app/src/Root.js
+++ web-app/src/Root.js
@@ -1,18 +1,14 @@
 import React from "react";
 import {Route, Switch} from "react-router-dom";
-
-// dev only
-import JSONPretty from 'react-json-pretty';
-
+import Escherpad from "./pages/Escherpad";
 
 export default function Root() {
     return (
         <Switch>
             <Route exact path="/" component={() => <div>path is /</div>}/>
-            <Route exact path="/all" component={() => <div>path is /all</div>}/>
-            <Route exact path="/:username/:bindr_id/:path*"
-                   component={(props) => <div>root page<JSONPretty json={props}/></div>}/>
-            <Route component={()=><div>404 no match</div>}/>
+            <Route exact path="/all" component={Escherpad}/>
+            <Route exact path="/:username/:bindr_id*/:note_id" component={Escherpad}/>
+            <Route component={() => <div>404 no match</div>}/>
         </Switch>
     );
 }
