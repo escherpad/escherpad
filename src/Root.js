e5013f06c0f666fc9204fefa039b29d3627b3792
Ge Yang <yangge1987@gmail.com>
2017-07-23 01:05:36 -0500

    remove page layout

--- src/Root.js
+++ src/Root.js
@@ -1,11 +1,8 @@
 import React from "react";
 import {Route} from "react-router-dom";
-import PageLayout from "./pages/layout/page-layout";
 
 export default function Root() {
     return (
-        <PageLayout>
-            <Route exact path="/" component={() => <div>root page</div>}/>
-        </PageLayout>
+        <Route exact path="/" component={() => <div>root page</div>}/>
     );
 }
