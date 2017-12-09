ea4b27fedcf0d1cd17c6800b91753322a5d8e592
Ge Yang <yangge1987@gmail.com>
2017-12-08 18:29:39 -0600

    Add server-side bundle splitting

--- web-app/server/ReactLoader.js
+++ web-app/server/ReactLoader.js
@@ -6,6 +6,8 @@ import {ServerStyleSheet, StyleSheetManager} from 'styled-components'
 import fs from 'fs';
 import {StyleSheet} from 'react-primitives';
 import {Helmet} from 'react-helmet';
+import {AsyncComponentProvider, createAsyncContext} from "react-async-component";
+import asyncBootstrapper from "react-async-bootstrapper";
 
 let rootComponentPath;
 if (process.env.NODE_ENV && process.env.NODE_ENV.toLowerCase() === "production") {
@@ -22,21 +24,26 @@ export default function ReactLoader(req, res, next) {
     const location = req.url.toString();
 
     const sheet = new ServerStyleSheet();
-    const html = renderToString(sheet.collectStyles(
+    const asyncContext = createAsyncContext();
+    const app = (
         <StaticRouter location={location} context={{}}>
-            <Root/>
-        </StaticRouter>
-    ));
-    const helmet = Helmet.renderStatic(); // use renderStatic to prevent memory leak
-    const styledComponentCSS = sheet.getStyleTags();
-    const reactPrimitiveCSS = StyleSheet
-        .getStyleSheets()
-        .map(({id, textContent}) => `<style id=${id}>${textContent}</style>`)
-        .join('');
-    res.status(200).send(
-        HTML
-            .replace(/<!-- SSR:title -->/, helmet.title.toString())
-            .replace(/<!-- SSR:CSS -->/, styledComponentCSS + reactPrimitiveCSS)
-            .replace(/<!-- SSR:HTML -->/, html)
-    );
+            <AsyncComponentProvider asyncContext={asyncContext}>
+                <Root/>
+            </AsyncComponentProvider>
+        </StaticRouter>);
+    asyncBootstrapper(app).then(() => {
+        const html = renderToString(sheet.collectStyles(app));
+        const helmet = Helmet.renderStatic(); // use renderStatic to prevent memory leak
+        const styledComponentCSS = sheet.getStyleTags();
+        const reactPrimitiveCSS = StyleSheet
+            .getStyleSheets()
+            .map(({id, textContent}) => `<style id=${id}>${textContent}</style>`)
+            .join('');
+        res.status(200).send(
+            HTML
+                .replace(/<!-- SSR:title -->/, helmet.title.toString())
+                .replace(/<!-- SSR:CSS -->/, styledComponentCSS + reactPrimitiveCSS)
+                .replace(/<!-- SSR:HTML -->/, html)
+        );
+    });
 }
--- web-app/src/Root.js
+++ web-app/src/Root.js
@@ -4,13 +4,17 @@ import Escherpad from "./pages/Escherpad";
 import NoMatch from "./pages/404.js";
 import FrontPage from "./pages/FrontPage";
 import Todo from "./pages/Todo";
-import Findr from "./pages/Findr";
+import {asyncComponent} from "react-async-component";
+
+const AsyncFindr = asyncComponent({
+    resolve: () => import('./pages/Findr')
+});
 
 export default function Root() {
     return (
         <Switch>
             <Route exact path="/" component={() => <FrontPage/>}/>
-            <Route exact path="/findr" component={Findr}/>
+            <Route exact path="/findr" component={AsyncFindr}/>
             <Route exact path="/todo" component={Todo}/>
             <Route exact path="/github/:account/gists/:path*" component={Escherpad}/>
             <Route exact path="/github/:account/:repo/:path*" component={Escherpad}/>
